e8b5daa0857ce652eaf8c0c4da15e4e30999e50c
Raju Komarapu <raju.k@keka.com>
2025-03-07 19:35:01 +0530

    Added context controller

---
+++ nexus-api/Controllers/ContextController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nexus.Core.Models;
+using Nexus.WebAPI.Controllers;
+
+namespace nexus_api.Controllers;
+
+[Route("api/context")]
+public class ContextController : BaseController
+{
+    private readonly IRequestContext RequestContext;
+    public ContextController(IRequestContext requestContext)
+    {
+        this.RequestContext = requestContext;
+    }
+
+    [HttpGet]
+    public IRequestContext GetContext()
+    {
+        return this.RequestContext;
+    }
+}
