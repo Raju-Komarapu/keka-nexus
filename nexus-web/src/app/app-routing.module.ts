7cf7681f9963519390e74c6957989fa2cce90460
Raju Komarapu <raju.k@keka.com>
2025-03-08 01:14:52 +0530

    added job component

--- nexus-web/src/app/app-routing.module.ts
+++ nexus-web/src/app/app-routing.module.ts
@@ -6,17 +6,19 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './auth/components/register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { authGuard } from './auth/route-guards/auth-guard';
+import { JobComponent } from './job/job.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'profile', pathMatch: 'full'},
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-    { path: 'profile', component: ProfileComponent, canActivate: [authGuard] }
+    { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+    { path: 'job', component: JobComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
