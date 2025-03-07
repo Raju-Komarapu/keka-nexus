25d95953812905e9fed75f6b5e0d1fe5eb3b13bb
Tharun Gade <tharun.g@keka.com>
2025-03-07 19:16:17 +0530

    adds required models for candidate profile

---
+++ Nexus.Core/Extensions/JsonExtensions.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="JsonExtensions.cs" company="Keka Inc">
+//     Copyright (c) Keka.com. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Text.Json;
+
+namespace Nexus.Core.Extensions;
+
+/// <summary>
+/// Represent a Json Extension.
+/// </summary>
+public static class JsonExtensions
+{
+    /// <summary>
+    /// Converts to json.
+    /// </summary>
+    /// <typeparam name="T">source type.</typeparam>
+    /// <param name="source">The source.</param>
+    /// <returns>
+    /// The Json string.
+    /// </returns>
+    public static string ToJson<T>(this T source)
+        where T : class
+    {
+        return source == default(T) ? string.Empty : JsonSerializer.Serialize(source);
+    }
+
+    /// <summary>
+    /// Froms the json.
+    /// </summary>
+    /// <typeparam name="T">destination type.</typeparam>
+    /// <param name="str">The string.</param>
+    /// <returns>
+    /// The object.
+    /// </returns>
+    public static T FromJson<T>(this string str)
+    {
+        return string.IsNullOrWhiteSpace(str) ? default : JsonSerializer.Deserialize<T>(str);
+    }
+}
---
+++ Nexus.Core/Models/EducationDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace Nexus.Core.Models;
+public class EducationDetails
+{
+    public string Degree { get; set; }
+
+    public string Specialization { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public string Score { get; set; }
+
+    public string InstitutionName { get; set; }
+
+    public string Location { get; set; }
+}
