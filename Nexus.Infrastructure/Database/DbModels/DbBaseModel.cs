fd8bd901f8f6d8befaa7f5177c9f3b959df7cbbe
Tharun Gade <tharun.g@keka.com>
2025-03-07 11:41:09 +0530

    project clean up

--- Nexus.Infrastructure/Database/DbModels/DbBaseModel.cs
+++ Nexus.Infrastructure/Database/DbModels/DbBaseModel.cs
@@ -2,18 +2,8 @@
 {
     internal abstract class DbBaseModel
     {
-        public Guid CreatedBy { get; set; }
-
         public DateTime CreatedOn { get; set; }
 
-        public Guid? UpdatedBy { get; set; }
-
         public DateTime? UpdatedOn { get; set; }
-
-        public bool IsDeleted { get; set; }
-
-        public Guid? DeletedBy { get; set; }
-
-        public DateTime? DeletedOn { get; set; }
     }
 }
