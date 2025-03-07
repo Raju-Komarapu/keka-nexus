daf7beb44c19cb07a2d3b3d5dae328cc506b0ab9
Tharun Gade <tharun.g@keka.com>
2025-03-07 23:16:29 +0530

    adds core model for job application

---
+++ Nexus.Core/Models/Enums/ApplicationStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Nexus.Core.Models.Enums;
+
+public enum ApplicationStatus
+{
+    New = 0,
+    Screening = 1,
+    Interview = 2,
+    Offered = 3,
+    Hired = 4,
+    Archived = 5
+}
---
+++ Nexus.Core/Models/JobApplication.cs
@@ -0,0 +1,14 @@
+﻿using Nexus.Core.Models.Enums;
+
+namespace Nexus.Core.Models;
+
+public class JobApplication
+{
+    public int Id { get; set; }
+    public Guid Identifier { get; set; }
+    public int JobId { get; set; }
+    public string TenantId { get; set; }
+    public int ProfileId { get; set; }
+    public ApplicationStatus ApplicationStatus { get; set; }
+    public DateTime AppliedOn { get; set; }
+}
