ca4bc0926221f610a7b6fb3db0b15c34cf921acb
Raju Komarapu <raju.k@keka.com>
2025-03-07 12:27:37 +0530

    Added Auth controller and services

---
+++ nexus-api/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nexus.Application.DTO;
+using Nexus.Application.Services.Interfaces;
+
+namespace nexus_api.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+[AllowAnonymous]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService AuthService;
+
+    public AuthController(IAuthService authService)
+    {
+        this.AuthService = authService;
+    }
+
+    [HttpPost("login")]
+    public string Login(LoginDTO loginDTO)
+    {
+        return this.AuthService.Login(loginDTO);
+    }
+
+    [HttpPost("Register")]
+    public string Register(RegisterDTO registerDTO)
+    {
+        return this.AuthService.Register(registerDTO);
+    }
+
+}
