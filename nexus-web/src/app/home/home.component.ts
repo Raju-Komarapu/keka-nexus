9ce317604c348b29e9b5260b3f08a5e7dc530ffd
Tharun Gade <tharun.g@keka.com>
2025-03-09 11:34:06 +0530

    fixed complete profile

--- nexus-web/src/app/home/home.component.ts
+++ nexus-web/src/app/home/home.component.ts
@@ -3,20 +3,22 @@ import { RouterModule } from '@angular/router';
 import { ContextService } from '../context/context-service';
 import { TopNavComponent } from '../shared/components/top-nav/top-nav.component';
 import { AuthService } from '../auth/services/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
-  imports: [TopNavComponent, RouterModule]
+  imports: [TopNavComponent, RouterModule, NgIf]
 })
 export class HomeComponent implements OnInit {
+    isContextInitilized: boolean;
     constructor(private contextService: ContextService,
                 private authService: AuthService) {}
     
     ngOnInit(): void {
        if (this.authService.hasToken()) {
-           this.contextService.initialize();
+           this.contextService.initialize().then(() => this.isContextInitilized = true);
        }
     }
 }
