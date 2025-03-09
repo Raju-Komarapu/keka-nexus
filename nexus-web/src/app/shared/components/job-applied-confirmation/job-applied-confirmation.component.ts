c0b2adbd0dc3a81d4ee4b46e3459c1adf5f7f73b
Tharun Gade <tharun.g@keka.com>
2025-03-09 13:12:35 +0530

    adds loading screen in submit application

--- nexus-web/src/app/shared/components/job-applied-confirmation/job-applied-confirmation.component.ts
+++ nexus-web/src/app/shared/components/job-applied-confirmation/job-applied-confirmation.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { NotificationService } from '../../services/notification.service';
 
@@ -6,11 +7,32 @@ import { NotificationService } from '../../services/notification.service';
     selector: 'job-applied-confirmation',
     templateUrl: './job-applied-confirmation.component.html',
     standalone: true,
-    imports: []
+    imports: [NgIf]
 })
-export class JobAppliedConfirmation {
-    private screeningUrl="https://tharungade.in";
+export class JobAppliedConfirmation implements OnInit{
+    private screeningUrl="https://interview.screeners.ai/signin/67b2e10f60c1598cdd873d14";
     screeningQuestionsWindow: Window;
+    isRequestInProgress: boolean;
+    processStepIndex: number;
+    intervalId: any;
+    processSteps = [
+      'Submitting your application...',
+      'Reviewing your application...',
+      "Congratulations! You've been shortlisted!"
+    ]
+
+    ngOnInit(): void {
+      this.isRequestInProgress = true;
+      this.processStepIndex = 0;
+      this.intervalId = setInterval(() => { 
+        this.processStepIndex = (this.processStepIndex + 1) % 3 
+        if(this.processStepIndex == 2)
+        {
+          this.isRequestInProgress = false;
+          clearInterval(this.intervalId)
+        }
+      }, 2000);
+    }
 
     sendConfirmation: EventEmitter<boolean> = new EventEmitter<boolean>();
 
